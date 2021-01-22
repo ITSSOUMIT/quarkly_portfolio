4df1352a665be68442a2c2cde2e0e321341f65f8
Soumit Das <55306758+ITSSOUMIT@users.noreply.github.com>
2021-01-22 22:07:54 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"600afedddaacc2001e8f947e",
+				"600afedddaacc2001e8f9480"
+			]
+		},
+		"600afedddaacc2001e8f947e": {
+			"id": "600afedddaacc2001e8f947e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"600afedddaacc2001e8f9480": {
+			"id": "600afedddaacc2001e8f9480",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
